9fdf7eaa50c3d5809dfa4fc254d11e6a2363fa7b
Muppens <hampusdunstrom@gmail.com>
2015-04-14 13:47:24 +0200

    temporary fix to menu nut returning by adding a sout call in the while loop.

--- src/entity/AbstractRobot.java
+++ src/entity/AbstractRobot.java
@@ -1,7 +1,6 @@
 package entity;
 
 import javax.swing.*;
-import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
--- src/game/Menu.java
+++ src/game/Menu.java
@@ -22,94 +22,96 @@ public class Menu extends JFrame {
     private boolean done = false;
 
     public Menu() {
-	super("RoboRampage");
+        super("RoboRampage");
 
-	playerNames = new ArrayList<>();
-	playerColors = new ArrayList<>();
+        playerNames = new ArrayList<>();
+        playerColors = new ArrayList<>();
 
     }
 
-    private void addPlayer(){
+    private void addPlayer() {
 
-	JPanel playerPanel = new JPanel();
-	Label playerNr = new Label("Player " + numberOfPlayers + ":");
+        JPanel playerPanel = new JPanel();
+        Label playerNr = new Label("Player " + numberOfPlayers + ":");
 
-	JTextField playerName = new JTextField("Player " + numberOfPlayers);
-	playerNames.add(playerName);
-	playerName.setColumns(10);
+        JTextField playerName = new JTextField("Player " + numberOfPlayers);
+        playerNames.add(playerName);
+        playerName.setColumns(10);
 
-	JButton playerRobot = new JButton("Robot");
-	String[] colors = {"Gray", "Blue", "Yellow", "Green", "Red"};
-	JComboBox<String> playerColor = new JComboBox<>(colors);
-	playerColor.setSelectedIndex(numberOfPlayers - 1);
-	playerColors.add(playerColor);
+        JButton playerRobot = new JButton("Robot");
+        String[] colors = {"Gray", "Blue", "Yellow", "Green", "Red"};
+        JComboBox<String> playerColor = new JComboBox<>(colors);
+        playerColor.setSelectedIndex(numberOfPlayers - 1);
+        playerColors.add(playerColor);
 
-	playerPanel.add(playerNr);
-	playerPanel.add(playerName);
-	playerPanel.add(playerRobot);
-	playerPanel.add(playerColor);
+        playerPanel.add(playerNr);
+        playerPanel.add(playerName);
+        playerPanel.add(playerRobot);
+        playerPanel.add(playerColor);
 
-	playerSelect.add(playerPanel);
+        playerSelect.add(playerPanel);
     }
 
     public List<Player> setUpPlayers() {
 
-	final int height = 400;
-	final int width = 400;
+        final int height = 400;
+        final int width = 400;
 
-	setLayout(new BorderLayout());
+        setLayout(new BorderLayout());
 
-	setResizable(false);
+        setResizable(false);
 
-	Label title = new Label("ROBO RAMPAGE");
-	playerSelect = new JPanel();
-	JButton addPlayerBtn = new JButton("Add Player");
-	JButton startBtn = new JButton("START");
+        Label title = new Label("ROBO RAMPAGE");
+        playerSelect = new JPanel();
+        JButton addPlayerBtn = new JButton("Add Player");
+        JButton startBtn = new JButton("START");
 
-	add(title, BorderLayout.PAGE_START);
+        add(title, BorderLayout.PAGE_START);
 
-	playerSelect.add(addPlayerBtn);
+        playerSelect.add(addPlayerBtn);
 
-	addPlayer();
+        addPlayer();
 
-	add(playerSelect, BorderLayout.CENTER);
+        add(playerSelect, BorderLayout.CENTER);
 
-	add(startBtn, BorderLayout.PAGE_END);
+        add(startBtn, BorderLayout.PAGE_END);
 
-	setSize(width, height);
-	requestFocus();
-	setVisible(true);
+        setSize(width, height);
+        requestFocus();
+        setVisible(true);
 
-	addPlayerBtn.setAction(new AbstractAction()
-	{
-	    @Override public void actionPerformed(ActionEvent e) {
-		if (numberOfPlayers < MAXPLAYERS) {
-		    numberOfPlayers++;
-		    addPlayer();
+        addPlayerBtn.setAction(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (numberOfPlayers < MAXPLAYERS) {
+                    numberOfPlayers++;
+                    addPlayer();
 
-		    requestFocus();
-		    setVisible(true);
-		}
-	    }
-	});
+                    requestFocus();
+                    setVisible(true);
+                }
+            }
+        });
 
-	startBtn.setAction(new AbstractAction()
-	{
-	    @Override public void actionPerformed(ActionEvent e) {
-		if(playerNames.size() > 1) {
-		    done = true;
-		}
-	    }
-	});
+        startBtn.setAction(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (playerNames.size() > 1) {
+                    done = true;
+                }
+            }
+        });
 
-	addPlayerBtn.setText("Add Player");
-	startBtn.setText("START");
+        addPlayerBtn.setText("Add Player");
+        startBtn.setText("START");
 
-	while(true) {
-	    if(done) {
-		return PlayerFactory.createPlayers(playerNames, playerColors, numberOfPlayers);
-	    }
-	}
+        while (true) {
+            if (done) {
+                return PlayerFactory.createPlayers(playerNames, playerColors, numberOfPlayers);
+            }else {
+                System.out.println();
+            }
+        }
 
     }
 
