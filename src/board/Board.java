941950d5d4ba5d66a1702e3f3af24013e608b1e3
Muppens <hampusdunstrom@gmail.com>
2015-04-21 16:49:26 +0200

    Fixed small error in update tiles

--- src/board/Board.java
+++ src/board/Board.java
@@ -367,7 +367,7 @@ public class Board {
     private void updateTiles(){
 
 	for (AbstractRobot robot : robots) {
-	    Tile tile = tiles[robot.getX()][robot.getY()];
+	    Tile tile = tiles[robot.getX() / tileSize][robot.getY() / tileSize];
 	    tile.update(robot);
 
 	    if(tile.willMoveRobot() && canMoveRobot(robot)){ // check if move to tempx and tempy is possible
